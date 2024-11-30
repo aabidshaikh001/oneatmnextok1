2f94afdaa1c62b933fa07a5ac8fecdefb97b6fa0
aabidshaikh001 <technicalsheikhshabb@gmail.com>
2024-11-30 17:19:28 +0530

    okk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/logox.png" />
+
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
